1d860288808a79db71ee367db57783cd09d9bd14
mfirmanakbar <em.firman.akbar@gmail.com>
2023-04-07 21:23:07 +0700

    show list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { ProductList } from './components/ProductList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Product List</h1>
+      <ProductList />
     </div>
   );
 }
