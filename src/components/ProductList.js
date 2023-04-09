5b082bca070832654e5eac85e26c1ca63dba5cc8
mfirmanakbar <em.firman.akbar@gmail.com>
2023-04-09 09:10:47 +0700

    handling error

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,20 +1,20 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react"
 import { useFetch } from "../hooks/useFetch";
 
 export const ProductList = () => {
     const [url, setUrl] = useState("http://localhost:8000/products/");
-    const { data: products, loading } = useFetch(url);
-    console.log(products);
+    const { data: products, loading, error } = useFetch(url);
 
-     return (
+  return (
     <section>
         <div className="filter">
             <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
             <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock Only</button>
         </div>
-        
         { loading && <p>Loading products...</p>}
 
+        { error && <p>{error}</p>}
+        
         { products && products.map((product) => (
             <div className="card" key={product.id}>
                 <p className="id">{product.id}</p>
@@ -23,7 +23,7 @@ export const ProductList = () => {
                     <span>${product.price}</span>
                     <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
                 </p>
-            </div>
+            </div>            
         )) }
     </section>
   )
