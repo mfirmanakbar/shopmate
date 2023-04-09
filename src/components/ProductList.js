65965a892183d946719e656db682cac3d405ec6c
mfirmanakbar <em.firman.akbar@gmail.com>
2023-04-09 08:59:27 +0700

    adding loading

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,16 +3,18 @@ import { useFetch } from "../hooks/useFetch";
 
 export const ProductList = () => {
     const [url, setUrl] = useState("http://localhost:8000/products/");
-    const { data: products } = useFetch(url);
+    const { data: products, loading } = useFetch(url);
     console.log(products);
 
-    return (
+     return (
     <section>
         <div className="filter">
             <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
             <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock Only</button>
         </div>
-         
+        
+        { loading && <p>Loading products...</p>}
+
         { products && products.map((product) => (
             <div className="card" key={product.id}>
                 <p className="id">{product.id}</p>
