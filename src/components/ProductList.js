9780c0f48b8f160bfa21373cae72ad63f69c16d3
mfirmanakbar <em.firman.akbar@gmail.com>
2023-04-09 08:44:15 +0700

    adding custome hook

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,28 +1,19 @@
 import { useCallback, useEffect, useState } from "react";
+import { useFetch } from "../hooks/useFetch";
 
 export const ProductList = () => {
-    const [products, setProducts] = useState([]);
-    const [url, setUrl] = useState("http://localhost:8000/products");
+    const [url, setUrl] = useState("http://localhost:8000/products/");
+    const { data: products } = useFetch(url);
+    console.log(products);
 
-    const fetchProducts = useCallback(async () => {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log("data: " + data);
-        setProducts(data);
-    }, [url]);
-    
-    useEffect(() => {
-        fetchProducts();
-        console.log("-");
-    }, [fetchProducts]);
-
-  return (
+    return (
     <section>
         <div className="filter">
             <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
             <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock Only</button>
         </div>
-        { products.map((product) => (
+         
+        { products && products.map((product) => (
             <div className="card" key={product.id}>
                 <p className="id">{product.id}</p>
                 <p className="name">{product.name}</p>
